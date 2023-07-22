73d30360b68c4decd3f0ba3911895c2aa9f5a6c6
Emma <ing.valen0421@hotmail.com>
2023-07-22 13:18:24 -0400

    feat: configure and implementing docker, docker-compose and pipelines configurations for gitlab-ci and azure-pipelines yml

---
+++ setup_env.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+env=$1
+
+TAG_IMAGE=TAG_IMAGE_$env
+NODE_ENV=NODE_ENV_$env
+PORT=PORT_$env
+
+VITE_REACT_APP_API_URL=REACT_APP_API_URL_$env
+VITE_REACT_APP_API_KEY=REACT_APP_API_KEY_$env
+
+echo TAG_IMAGE=${!TAG_IMAGE} >> .env
+echo NODE_ENV=${!NODE_ENV} >> .env
+echo PORT=${!PORT} >> .env
+
+echo VITE_REACT_APP_API_URL=${!VITE_REACT_APP_API_URL} >> .env
+echo VITE_REACT_APP_API_KEY=${!VITE_REACT_APP_API_KEY} >> .env
